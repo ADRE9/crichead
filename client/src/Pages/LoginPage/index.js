b9277a3d7ae0c49960a7f2ba80df798ddf78bbf5
Adrenine <adrenine13@gmail.com>
2021-04-24 12:21:33 +0530

    LoginPage added

---
+++ client/src/Pages/LoginPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const OuterDiv = styled.div`
+`;
+
+const index = () => {
+  return (
+    <OuterDiv>
+      
+    </OuterDiv>
+  )
+}
+
+export default index;
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Navbar from './Navbar';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import {facebookProvider} from "../configs/firebaseHelper";
 
 class App extends React.Component {
 
+  // setUpRecaptcha = () => {
+  //   window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
+  //     'size': 'invisible',
+  //     'callback': (response) => {
+  //       // reCAPTCHA solved, allow signInWithPhoneNumber.
+  //       this.onSignInSubmit();
+  //     }
+  //   });    
+  // }
+
+  // onSignInSubmit = () => {
+    
+  // }
+
   render() {
-    console.log(facebookProvider)
     return (
       <Navbar/>
     )

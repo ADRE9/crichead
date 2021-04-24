0cdbae4c2119c342ae3f0fde5a8580097924b644
Adrenine <adrenine13@gmail.com>
2021-04-24 14:08:35 +0530

    resolved conflicts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,27 +4,18 @@ import {facebookProvider} from "../configs/firebaseHelper";
 import  {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from '../Pages/LandingPage';
 import { SigninPage } from '../Pages/LandingPage/signin/signin';
-
-import { BrowserRouter as Router } from "react-router-dom";
-import { Route,Switch } from "react-router-dom";
+import Navbar from "./Navbar";
 
 class App extends React.Component {
 
   render() {
     return (
       <Router>
-
+        <Navbar />
         <Switch>
           <Route path="/" component={Home} exact/>
           <Route path="/signin" component={SigninPage} exact/>
-        </Switch>
-        <Navbar />
-        <Switch>
-          <Route>
-            
-          </Route>
-        </Switch>
-        
+        </Switch> 
       </Router>
     )
   }

bbc02863eb8ca686991014c23444d56b7b7975e4
Adrenine <adrenine13@gmail.com>
2021-04-24 12:51:45 +0530

    some minor changes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import Navbar from './Navbar';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
 import {facebookProvider} from "../configs/firebaseHelper";
-import  {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route,Switch } from "react-router-dom";
+
 class App extends React.Component {
-
-  // setUpRecaptcha = () => {
-  //   window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('sign-in-button', {
-  //     'size': 'invisible',
-  //     'callback': (response) => {
-  //       // reCAPTCHA solved, allow signInWithPhoneNumber.
-  //       this.onSignInSubmit();
-  //     }
-  //   });    
-  // }
-
-  // onSignInSubmit = () => {
-    
-  // }
 
   render() {
     return (
       <Router>
-        <Navbar/>
+        <Navbar />
+        <Switch>
+          <Route>
+            
+          </Route>
+        </Switch>
+        
       </Router>
       
     )

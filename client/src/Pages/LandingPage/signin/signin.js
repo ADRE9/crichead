bc2849e63f92677552a902cd008df6c06e1cf02c
lopamudra2000 <lopamudragiri2@gmail.com>
2021-04-24 13:35:04 +0530

    added signin

---
+++ client/src/Pages/LandingPage/signin/signin.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Signin } from '.'
+
+export const SigninPage = () => {
+    return (
+        <Signin/>
+    )
+}
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import Navbar from './Navbar';
 import './App.css';
 import {facebookProvider} from "../configs/firebaseHelper";
-import  {BrowserRouter as Router} from "react-router-dom";
+import  {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Home from '../Pages/LandingPage';
+import { SigninPage } from '../Pages/LandingPage/signin/signin';
 class App extends React.Component {
 
   render() {
     console.log(facebookProvider)
     return (
       <Router>
-        <Navbar/>
+        <Switch>
+          <Route path="/" component={Home} exact/>
+          <Route path="/signin" component={SigninPage} exact/>
+        </Switch>
       </Router>
-      
     )
   }
 };
